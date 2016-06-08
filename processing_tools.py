c5d86e23500677d087e233cad5a32b96ca23193d
Simon Vedrine <simon.vedrine@u-psud.fr>
2016-06-08 17:02:35 +0200

    Add dewow_filtering function. Others had been improved for sake of clarity

--- processing_tools.py
+++ processing_tools.py
@@ -3,26 +3,38 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 from functions import *
 
-def time_zero (data, dtdx, t0=0.0):
-	"""Place the initial time of your radargrams at t0 """
 
-	dt, dx = dtdx
-	iterations, taces = data.shape
-	t = np.linspace(0, 1, iterations) * (iterations * dt)
+def dewow_filtering(data, dx_dt, time_window):
 	
-	index = 0	
-	while t0 > t[index]: 
-		index += 1
+	dx, dt = dx_dt
+	iterations, traces = data.shape
+	t = np.linspace(0, 1, iterations) * (dt * iterations)
+	width = int(time_window / dt)
 	
-	return data[index:]
+	for trace in traces:
+		index = 0
 		
-def user_gain (data, dtdx, gain='', start=0.0, stop=0.0):
-	"""Add a user defined gain chosen beetween {'constant', 'linear', 'enxponential'} on data.
-	start and stop define the number of traces you want modify.
-	"""
-	dt, dx = dtdx
+	
+def time_zero (data, dx_dt, t0=0.0):
+	"""Reshape start time of your radargrams at t0 """
+
+	dx, dt = dx_dt
 	iterations, traces = data.shape
 	t = np.linspace(0, 1, iterations) * (iterations * dt)
+	index = int(t0 / dt)
+
+	return data[index:]
+		
+def user_gain (data, dx_dt, gain='', start=0.0, stop=0.0):
+	"""Add a user defined gain chosen beetween {'constant', 'linear', 'enxponential'} on data.
+	start and stop define the number of traces you want to modify.
+	"""
+	dx, dt = dx_dt
+	iterations, traces = data.shape
+	t = np.linspace(0, 1, iterations) * (iterations * dt)
+	
+	start = int(start / dt)
+	stop = int(stop / dt)
 	
 	width = stop - start
 	
@@ -48,30 +60,28 @@ def user_gain (data, dtdx, gain='', start=0.0, stop=0.0):
 	for model in range(traces):
 		data[start:stop, model] *= np.array(fgain, dtype=data.dtype)
 
-def velocity_analysis (data, dtdx, param, start, stop):
+def velocity_analysis (data, dx_dt, param, start, stop):
 	"""
 	Plot the radargram along with the hyperbolic function initialized by x0,t0,c and r.
 	"""
 
 	traces = data.shape[1]	
-	dx = dtdx[1]
+	dx = dx_dt[1]
 	x0, t0, v, r = param
 	z0 = (t0 * v + 2 * r) / 2
 	x = np.linspace(0, 1, traces) * (traces * dx)
 	
 	hyperbol =  (2 / v) * (np.sqrt((x0-x[start:stop])**2 + z0**2) - r) 
 	
-	fig = plt.figure(num='radargrams', figsize=(20, 10), facecolor='w', edgecolor='w')
 	plt.plot(x[start:stop], hyperbol)
-	plot_radargrams(data, dtdx)
-	
-def stolt_migration(data, param):
+	plot_radargrams(data, dx_dt, 'velocity_analysis')
+
+def stolt_migration(data, dx_dt, c):
 	
 	# parameters
-	dx = param[1]
-	dt = param[0]
+	dx, dt = dx_dt
 	fs = 1/dt
-	
+	eps = 1e-4
 	nt0, nx0 = data.shape
 	t = np.linspace(0,nt0*dt,nt0) 
 	x = np.linspace(0,nx0*dx,nx0)
@@ -81,23 +91,26 @@ def stolt_migration(data, param):
 	nx = 2 * nx0
 	
 	# One Emiter-Receiver scenario
-	c = param[2]
 	ERMv = c / 2
 	
 	# FFT & shift 
 	fftdata = np.fft.fftshift(np.fft.fft2(data, s=(nt,nx)))
-
+	
+    
 	# Linear interpolation
 	f = np.linspace(-nt/2, nt/2-1, nt) * fs / nt 
 	kx = np.linspace(-nx/2,nx/2-1, nx) / dx / nx
 	kx, f = np.meshgrid(kx, f)
+	
+	# Remove evanescent parts
+	evanescent = (abs(f)  / abs(kx+eps) > c).astype(int)
+	fftdata *= evanescent
 	
 	# Remapping
 	fkz = ERMv*np.sign(f)*np.sqrt(kx**2 + f**2/ERMv**2)
 	fftdata = griddata((kx.ravel(), f.ravel()), fftdata.ravel(), (kx, fkz), method='nearest')
 	
 	# Jacombien
-	eps = 1e-4
 	fftdata *= f / (np.sqrt(kx**2 + f**2/ERMv**2)+eps)
 	
 	# IFFT & Migrated RF
@@ -106,7 +119,10 @@ def stolt_migration(data, param):
 	
 	z = t*c / 2
 	
-	fig = plt.figure(num='', figsize=(20, 10), facecolor='w', edgecolor='w')
+	fig = plt.figure(num='migrated_data', figsize=(20, 10), facecolor='w', edgecolor='w')
 	plt.imshow(abs(mig), extent=[np.amin(x), np.amax(x), np.amax(z), np.amin(z)], interpolation='nearest', aspect='auto', cmap='seismic', vmin=-np.amax(abs(mig)), vmax=np.amax(abs(mig)))
+	plt.xlabel('Distance [m]')
+	plt.ylabel('Depth [m]')
+	
 	plt.show()
 	
