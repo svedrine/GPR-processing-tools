ad69a3c272ae86b14ab969e6fea3bcae514cfd0e
Simon Vedrine <simon.vedrine@u-psud.fr>
2016-06-24 17:24:04 +0200

    Lastest version

--- processing_tools.py
+++ processing_tools.py
@@ -1,21 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import griddata, SmoothBivariateSpline
+from utils import *
+from scipy.interpolate import griddata
+from scipy.signal import medfilt2d, order_filter
 
-from functions import *
-
-
-def mean_filter(data, dx_dt, time_window):
+def mean_filter(data, dx_dt, window_width):
 	
-	""" Remove the DC bias by using a mean filter. time_window (ns)"""
+	""" Remove the DC bias by using a mean filter. window_width (ns)"""
 	
 	dx, dt = dx_dt
 	samples, traces = data.shape
 	t = np.linspace(0, 1, samples) * (dt * samples)
-	width = int(time_window / dt)
+	
+	width = int(window_width / dt)
 	half_width = int(width / 2)
 	temp = np.ones((samples, traces))
 	temp *= data
+	
 	for trace in range(traces):
 		for index in range(samples):
 			if index < half_width:
@@ -28,28 +27,32 @@ def mean_filter(data, dx_dt, time_window):
 				
 	return data
 		
-def dc_substraction (data):
+def dc_substraction (data, dx_dt, time_window):
 	
-	""" Remove the DC bias by using a simple substraction of the DC offset"""
+	""" Remove the DC bias by using a simple substraction of the DC offset
+	time_window is a tuple (start, stop) in ns."""
+	dt = dx_dt[1]
+	start, stop = time_window
+	start = int(start / dt)
+	stop = int(stop / dt)
 	
-	return data - np.mean(abs(data[int(0.67*len(data))]))	
+	return data - np.mean(abs(data[start:stop]))	
 	
-def cut_off_frequency(data, dx_dt, fc = 0.0):
+def cut_off_frequency(data, dx_dt, fc):
 	
-	""" Remove the DC bias by using Fast Fourier Transform. fc (MHz) must be bellow the bandwidth of the recorded data  ~ 10 MHz """
+	""" Apply a pass-band filter by using Fast Fourier Transform. fc (MHz) must be bellow the bandwidth of the recorded data  ~ 10 MHz """
 	
 	dx, dt = dx_dt
 	samples, traces = data.shape
+	f = np.linspace(0, 1, samples) * (samples / dt)
 	
 	index = int(fc * dt)
-	
 	fftdata = np.fft.fft2(data)
 	for trace in range(traces):
 		fit = np.diff(fftdata.real[index:index+2, trace]) * [range(index)]
 		fftdata.real[:index, trace] = fit
-		
+	
 	data = np.fft.ifft2(fftdata)
-
 	return data.real
 	
 def time_zero (data, dx_dt, t0 = 0.0):
@@ -63,71 +66,75 @@ def time_zero (data, dx_dt, t0 = 0.0):
 
 	return data[index:]
 		
-def user_gain (data, dx_dt, gain, time_window):
+def user_gain (data, dx_dt, sort, param, time_window, plot=False):
 	
 	"""Add a user defined gain chosen beetween {'constant', 'linear', 'enxponential'} on data.
-	time_window is a tuple (start (ns) , stop (ns) )
+	param is a tuple (a, b) where --> constant : fgain = a, linear : fgain = a*t, exponential : fgain = a*exp(b*t)
+	time_window is a tuple (start, stop) in ns.
 	"""
 	dx, dt = dx_dt
 	samples, traces = data.shape
+	a, b = param
 	t = np.linspace(0, 1, samples) * (samples * dt)
-	start, stop = time_window
+	t0, stop = time_window
 	
-	start = int(start / dt)
+	start = int(t0 / dt)
 	stop = int(stop / dt)
-	width = stop - start
+	width = start-stop
+	fgain = np.ones(samples)
 	
-	if gain == 'constant':
-		#check = input('Please enter a constant value : ')
-		#constant = float(check)
-		constant = 4
-		fgain = [constant]*width
+	if sort == 'constant':
+		fgain[start:stop] = [a]*width
 		
-	elif gain == 'linear':
-		check = input('Please enter gradient value: ')
-		gradient = float(check)
-		fgain = [gradient*1e9*t for time in t[start:stop]]
+	elif sort == 'linear':
+		fgain[start:stop] = [a*(t-t0) + 1 for t in t[start:stop]]
 
-	elif gain == 'exponential':
-		check = input('fgain = A * exp(B*t). Please enter A, B values: ')
-		A = float(check[0])
-		B = float(check[2])
-		fgain = [A*np.exp(B*t) for time in t[start:stop]]
-	else:
-		fgain = [1]*width
+	elif sort == 'exponential':
+		fgain[start:stop] = [a*(np.exp(b*(t-t0))-1) for t in t[start:stop]]
 		
-	for model in range(traces):
-		data[start:stop, model] *= np.array(fgain, dtype=data.dtype)
+	for trace in range(traces):
+		data[:, trace] *= fgain.astype(dtype=data.dtype)
+	
+	if plot is True:
+		plt.plot(fgain)
+		plt.show()
 	
 	return data
 
-def velocity_analysis (data, dx_dt, param, start, stop):
+def velocity_analysis (data, dx_dt, param, width):
 	"""
 	Plot the radargram along with the hyperbolic function initialized by a tuple = (x0 (m), t0 (ns), c (m/ns), r (m)).
 	"""
 
-	traces = data.shape[1]	
-	dx = dx_dt[1]
+	samples, traces = data.shape
+	dx, dt = dx_dt
 	x0, t0, v, r = param
+	print(dx)
+	mid = int(x0 / dx)
+	start = int(mid - width / (2 * dx))
+	stop = int(mid + width / (2 * dx))
+	print(start, stop)
 	z0 = (t0 * v + 2 * r) / 2
 	x = np.linspace(0, 1, traces) * (traces * dx)
-	
+	t = np.linspace(0, 1, samples) * (samples * dt)
 	hyperbol =  (2 / v) * (np.sqrt((x0-x[start:stop])**2 + z0**2) - r) 
 	
-	plt.plot(x[start:stop], hyperbol)
-	plot_radargram(data, dx_dt, 'velocity_analysis')
-
+	fig, ax = plt.subplots(num='velocity_analysis', figsize=(20, 10), facecolor='w', edgecolor='w')
+	ax.imshow(data, extent=[np.amin(x), np.amax(x), np.amax(t), np.amin(t)], interpolation='nearest', aspect='auto', cmap='seismic', vmin=-np.amax(abs(data)), vmax=np.amax(abs(data)))
+	ax.plot(x[start:stop], hyperbol)
+	plt.show()
+	
 def stolt_migration(data, dx_dt, c):
 	
-	# parameters
+	# Imput parameters
 	dx, dt = dx_dt
 	fs = 1/dt
-	eps = 1e-4
+	eps = 2.2e-16
 	nt0, nx0 = data.shape
 	t = np.linspace(0,nt0*dt,nt0) 
 	x = np.linspace(0,nx0*dx,nx0)
 	
-	# Zero-padding
+	# Zero-padding 2D
 	nt = 2 * nextpower(nt0)
 	nx = 2 * nx0
 	
@@ -137,8 +144,7 @@ def stolt_migration(data, dx_dt, c):
 	# FFT & shift 
 	fftdata = np.fft.fftshift(np.fft.fft2(data, s=(nt,nx)))
 	
-    
-	# Linear interpolation
+	# Build (kx, f) 
 	f = np.linspace(-nt/2, nt/2-1, nt) * fs / nt 
 	kx = np.linspace(-nx/2,nx/2-1, nx) / dx / nx
 	kx, f = np.meshgrid(kx, f)
@@ -147,23 +153,24 @@ def stolt_migration(data, dx_dt, c):
 	evanescent = (abs(f)  / abs(kx+eps) > c).astype(int)
 	fftdata *= evanescent
 	
-	# Remapping
+	# Stolt remapping function f(kz)
 	fkz = ERMv*np.sign(f)*np.sqrt(kx**2 + f**2/ERMv**2)
+	
+	# Linear interpolation on grid
 	fftdata = griddata((kx.ravel(), f.ravel()), fftdata.ravel(), (kx, fkz), method='nearest')
 	
 	# Jacombien
 	fftdata *= f / (np.sqrt(kx**2 + f**2/ERMv**2)+eps)
 	
-	# IFFT & Migrated RF
+	# IFFT & Migrated data 
 	mig = np.fft.ifft2(np.fft.ifftshift(fftdata))
-	mig = mig[1:nt0,1:nx0]
+	mig = mig[:nt0,:nx0]
 	
-	z = t * c / 2
+	dz = dt * c / 2
+	dx_dz = (dx, dz)
 	
-	fig = plt.figure(num='migrated_data', figsize=(20, 10), facecolor='w', edgecolor='w')
-	plt.imshow(abs(mig), extent=[np.amin(x), np.amax(x), np.amax(z), np.amin(z)], interpolation='nearest', aspect='auto', cmap='seismic', vmin=-np.amax(abs(mig)), vmax=np.amax(abs(mig)))
-	plt.xlabel('Distance [m]')
-	plt.ylabel('Depth [m]')
+	return abs(mig), dx_dz
 	
-	plt.show()
+	
+	
 	
